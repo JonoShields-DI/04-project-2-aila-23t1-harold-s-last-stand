af84892b4ee181ae82ca88a0581e26c32722c590
Deshan <deshan.mclachlan@developersinstitute.co.nz>
2023-03-21 15:13:56 +1300

    Formated files with prettier

--- client/src/components/CourseList.js
+++ client/src/components/CourseList.js
@@ -1,20 +1,20 @@
 import Course from "./Course";
 
 const CourseList = ({ courses }) => {
-	return (
-		<ul>
-			{courses.map((course) => {
-				<Course
-					name={course.name}
-					description={course.description}
-					price={course.price}
-					location={course.location}
-					teachers={course.teachers}
-					key={course.id}
-				/>;
-			})}
-		</ul>
-	);
+  return (
+    <ul>
+      {courses.map((course) => {
+        <Course
+          name={course.name}
+          description={course.description}
+          price={course.price}
+          location={course.location}
+          teachers={course.teachers}
+          key={course.id}
+        />;
+      })}
+    </ul>
+  );
 };
 
 export default CourseList;
