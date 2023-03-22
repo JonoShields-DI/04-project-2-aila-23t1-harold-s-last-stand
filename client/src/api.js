fdf20d104f524eb8589b78ebd3bde8c52fa5dfe9
Jen <jennnifer.corbett@developersinstitute.co.nz>
2023-03-23 11:57:15 +1300

    Update api

--- client/src/api.js
+++ client/src/api.js
@@ -1,28 +1,23 @@
 const api = {
   addNewCourse: async () => {
-    const REACT_APP_URL = `http://localhost:5001`;
     const course = {
       name: "Harold's Last Course",
-      description: "You thought you knew everything, didn't you?",
+      description: "Another course description",
       price: 5000000.0,
     };
 
-    await fetch(
-      // `/`,
-      `${REACT_APP_URL}/`,
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        courses: JSON.stringify(course, [
-          course.name,
-          course.description,
-          course.price,
-        ]),
-      }
-    );
+    await fetch(`/`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      courses: JSON.stringify(course, [
+        course.name,
+        course.description,
+        course.price,
+      ]),
+    });
 
     const newCourse = {
       course,
@@ -35,16 +30,11 @@ const api = {
   },
 
   getCourses: async () => {
-    const REACT_APP_URL = `http://localhost:5001`;
-    const response = await fetch(
-      // `/`,
-      `${REACT_APP_URL}/`,
-      {
-        method: "GET",
-      }
-    );
+    const response = await fetch(`/`, {
+      method: "GET",
+    });
     const result = await response.json();
-
+    console.log(result);
     return result;
   },
 };
