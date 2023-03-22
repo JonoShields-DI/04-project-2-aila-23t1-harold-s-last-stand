a1b5dc390aff1d3a5f114aeaf10b660c55dfbd14
Jen <jennnifer.corbett@developersinstitute.co.nz>
2023-03-22 16:44:24 +1300

    Change proxy to / rather than /test

--- client/src/setUpProxy.js
+++ client/src/setUpProxy.js
@@ -2,17 +2,10 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/test",
+    "/",
     createProxyMiddleware({
       target: process.env.API_URL,
       changeOrigin: true,
     })
   );
-    // app.use(
-    //   "/",
-    //   createProxyMiddleware({
-    //     target: process.env.API_URL,
-    //     changeOrigin: true,
-    //   })
-    // );
 };
